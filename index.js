40c223e8b6b93772d25624c19db11be3f5be873b
shani <shhhaniburde@gmail.com>
2021-12-13 16:41:04 -0500

    commit tree aa6e53ad958c69d2a940162c143106500d97a26e

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting;
+  if (feet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (feet > 2000 && feet < 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (feet >= 2500) {
+    greeting = 'No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+
+  return (city === 'NYC' ? "Ok, sounds good." : "No go." );
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+      case 'generous': return 'Thank you so much.';
+        break;
+      case 'not as generous': return 'Thank you.';
+        break;
+      default: return 'Bye.';
+    }
 }
